2361f2623a8dce23920707d8c7449ac2bb4f2c8f
Efimov-Alex <fastic2018l@yandex.ru>
2026-01-04 14:14:59 +0300

    Add admin

--- geo_point/points/admin.py
+++ geo_point/points/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import LocationPoint, PointMessage
 
-# Register your models here.
+@admin.register(LocationPoint)
+class LocationPointAdmin(admin.ModelAdmin):
+    list_display = ('name', 'latitude', 'longitude', 'address', 'created_at', 'get_coordinates_link')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'description', 'address')
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description')
+        }),
+        ('Координаты', {
+            'fields': ('latitude', 'longitude', 'address')
+        }),
+        ('Даты', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
+    
+    def get_coordinates_link(self, obj):
+        url = f"https://www.google.com/maps?q={obj.latitude},{obj.longitude}"
+        return format_html(f'<a href="{url}" target="_blank">🗺️ На карте</a>')
+    get_coordinates_link.short_description = 'Карта'
+
+@admin.register(PointMessage)
+class PointMessageAdmin(admin.ModelAdmin):
+    list_display = ('truncated_text', 'point', 'user', 'created_at')
+    list_filter = ('created_at', 'point')
+    search_fields = ('text', 'point__name', 'user__username')
+    readonly_fields = ('created_at',)
+    
+    def truncated_text(self, obj):
+        return obj.text[:50] + '...' if len(obj.text) > 50 else obj.text
+    truncated_text.short_description = 'Текст'
---
+++ geo_point/users/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'phone', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('username', 'email', 'phone')
+    ordering = ('username',)
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'phone')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'phone', 'password1', 'password2'),
+        }),
+    )
